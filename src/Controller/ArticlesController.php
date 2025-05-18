e531a2cad05f67aa7cb9842f7e6e554618b5c3e1
feugo161 <46138241+fuego161@users.noreply.github.com>
2025-05-18 21:44:54 +0100

    Add "add" action

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -15,4 +15,28 @@ class ArticlesController extends AppController
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact(('article')));
     }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+
+        if ($this->request->is('post')) {
+            $article = $this->Article->patchEntity($article, $this->request->getData());
+
+            debug($this->request->getData());
+
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+
+            $this->Flash->error(__('Unable to add your article'));
+
+        }
+
+        $this->set(compact(('article')));
+    }
 }
