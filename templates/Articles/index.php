a08d8a1d12f9714e2775bbf09166d3d2d4c9c00e
feugo161 <46138241+fuego161@users.noreply.github.com>
2025-05-18 22:12:32 +0100

    Add the ability to edit articles

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -12,10 +12,13 @@
     <?php foreach ($articles as $article): ?>
         <tr>
             <td>
-                <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+                <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]); ?>
             </td>
             <td>
                 <?= $article->created->format(DATE_RFC850); ?>
+            </td>
+            <td>
+                <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]); ?>
             </td>
         </tr>
     </tr>
